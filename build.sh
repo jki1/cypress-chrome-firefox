6af11fdfc301e6e0a31c2c18a557cb9b2676e6d6
Jonas Kiene <jonas.kiene@diva-e.com>
2022-02-01 14:30:13 +0100

    Added Dockerfile and build script, updated README.md

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set e+x
 IMAGE_NAME=jk2394/cypress-chrome-firefox
 
 echo "Building started for $IMAGE_NAME"
-docker build --no-cache -t IMAGE_NAME .
+docker build --no-cache -t $IMAGE_NAME .
 echo "Building completed for $IMAGE_NAME"
